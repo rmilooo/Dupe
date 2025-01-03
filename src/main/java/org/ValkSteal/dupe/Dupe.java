435452cb0fde2d868792d6fe8c67dedcf4e68976
rmilooo <73422068+rmilooo@users.noreply.github.com>
2025-01-04 00:55:41 +0100

    Initial commit

---
+++ src/main/java/org/ValkSteal/dupe/Dupe.java
@@ -0,0 +1,17 @@
+package org.ValkSteal.dupe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dupe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
