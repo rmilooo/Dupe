b9bd4fdaa023eb657a2360b8090cd8bc6ba8803a
rmilooo <73422068+rmilooo@users.noreply.github.com>
2025-01-04 01:04:18 +0100

    Added YMLHandler

---
+++ src/main/java/org/ValkSteal/dupe/YMLHandler.java
@@ -0,0 +1,59 @@
+package org.ValkSteal.dupe;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.File;
+import java.io.IOException;
+
+public class YMLHandler {
+    private final File file;
+    private final FileConfiguration config;
+
+    public YMLHandler(String fileName, String pluginFolder) {
+        this.file = new File(pluginFolder, fileName);
+        if (!file.exists()) {
+            try {
+                if (file.getParentFile().mkdirs()) {
+                    file.createNewFile();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        this.config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public String getString(String path, String defaultValue) {
+        return config.getString(path, defaultValue);
+    }
+
+    public int getInt(String path, int defaultValue) {
+        return config.getInt(path, defaultValue);
+    }
+
+    public boolean getBoolean(String path, boolean defaultValue) {
+        return config.getBoolean(path, defaultValue);
+    }
+
+    public ItemStack getItemStack(String path, ItemStack defaultValue) {
+        return config.getItemStack(path, defaultValue);
+    }
+
+    public void set(String path, Object value) {
+        config.set(path, value);
+    }
+
+    public void save() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+}
