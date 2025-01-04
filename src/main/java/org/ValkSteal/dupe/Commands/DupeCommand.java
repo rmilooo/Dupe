e5f336bb893abcff83f83c92cee8610e5d347f2c
rmilooo <73422068+rmilooo@users.noreply.github.com>
2025-01-04 01:22:04 +0100

    Things

--- src/main/java/org/ValkSteal/dupe/Commands/DupeCommand.java
+++ src/main/java/org/ValkSteal/dupe/Commands/DupeCommand.java
@@ -1,4 +1,15 @@
 package org.ValkSteal.dupe.Commands;
 
-public class DupeCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class DupeCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+
+        return false;
+    }
 }
--- src/main/java/org/ValkSteal/dupe/Dupe.java
+++ src/main/java/org/ValkSteal/dupe/Dupe.java
@@ -1,8 +1,11 @@
 package org.ValkSteal.dupe;
 
+import org.ValkSteal.dupe.Commands.DupeCommand;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public final class Dupe extends JavaPlugin {
@@ -12,6 +15,7 @@ public final class Dupe extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Sets the instance that can be accessed from anywhere in the plugin
         Instance = this;
 
 
@@ -25,8 +29,16 @@ public final class Dupe extends JavaPlugin {
     public void register(){
         // Register events and commands here
 
-
         // Commands:
+        Objects.requireNonNull(getCommand("dupe")).setExecutor(new DupeCommand());
+
+        // Permissions:
+        getServer().getPluginManager().addPermission(new org.bukkit.permissions.Permission("dupe.use"));
+        getServer().getPluginManager().addPermission(new org.bukkit.permissions.Permission("dupe.admin"));
+
+        // Config:
+        saveDefaultConfig();
+
 
         // Events:
     }
